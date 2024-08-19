d4a49c68b83d44159100d1d544c08da59f9baf5a
Arón Díaz <arondiaz.cpn@gmail.com>
2024-08-19 18:20:18 -0300

    showed data of products in cart

--- shop-cart-new/src/components/Header.jsx
+++ shop-cart-new/src/components/Header.jsx
@@ -1,10 +1,6 @@
+/* eslint-disable react/prop-types */
 
-const Header = () => {
-
-
-
-
- 
+const Header = ({ cart }) => {
   return (
     <header className="py-5 header">
       <div className="container-xl">
@@ -27,45 +23,51 @@ const Header = () => {
               />
 
               <div id="carrito" className="bg-white p-3">
-                <p className="text-center">El carrito esta vacio</p>
-                <table className="w-100 table">
-                  <thead>
-                    <tr>
-                      <th>Imagen</th>
-                      <th>Nombre</th>
-                      <th>Precio</th>
-                      <th>Cantidad</th>
-                      <th></th>
-                    </tr>
-                  </thead>
-                  <tbody>
-                    <tr>
-                      <td>
-                        <img
-                          className="img-fluid"
-                          src="./public/img/guitarra_02.jpg"
-                          alt="imagen guitarra"
-                        />
-                      </td>
-                      <td>SRV</td>
-                      <td className="fw-bold"></td>
-                      <td className="flex align-items-start gap-4">
-                        <button type="button" className="btn btn-dark">
-                          -
-                        </button>
-                        1
-                        <button type="button" className="btn btn-dark">
-                          +
-                        </button>
-                      </td>
-                      <td>
-                        <button className="btn btn-danger" type="button">
-                          X
-                        </button>
-                      </td>
-                    </tr>
-                  </tbody>
-                </table>
+                {cart.length === 0 ? (
+                  <p className="text-center">El carrito esta vacio</p>
+                ) : (
+                  <table className="w-100 table">
+                    <thead>
+                      <tr>
+                        <th>Imagen</th>
+                        <th>Nombre</th>
+                        <th>Precio</th>
+                        <th>Cantidad</th>
+                        <th></th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      {cart &&
+                        cart.map((guitar, index) => (
+                          <tr key={index}>
+                            <td>
+                              <img
+                                className="img-fluid"
+                                src={`/img/${guitar.image}.jpg`}
+                                alt="imagen guitarra"
+                              />
+                            </td>
+                            <td>{guitar.name}</td>
+                            <td className="fw-bold">{guitar.price}</td>
+                            <td className="flex align-items-start gap-4">
+                              <button type="button" className="btn btn-dark">
+                                -
+                              </button>
+                              {guitar.quantity}
+                              <button type="button" className="btn btn-dark">
+                                +
+                              </button>
+                            </td>
+                            <td>
+                              <button className="btn btn-danger" type="button">
+                                X
+                              </button>
+                            </td>
+                          </tr>
+                        ))}
+                    </tbody>
+                  </table>
+                )}
 
                 <p className="text-end">
                   Total pagar: <span className="fw-bold">$899</span>
