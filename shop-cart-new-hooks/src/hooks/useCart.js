767bd18c14c6b4eaaed5492cc9a2509702325016
Arón Díaz <arondiaz.cpn@gmail.com>
2024-08-20 14:13:33 -0300

    duplicated project and implemented custom hook

---
+++ shop-cart-new-hooks/src/hooks/useCart.js
@@ -0,0 +1,82 @@
+import { useState, useEffect } from "react";
+import { db } from "../db/db";
+
+const useCart = () => {
+  const initialCart = () => {
+    const updateCart = localStorage.getItem("cart");
+    return updateCart ? JSON.parse(updateCart) : [];
+  };
+
+  const [guitar] = useState(db);
+  const [cart, setCart] = useState(initialCart);
+
+  // useEffect(() => {
+  //   setGuitar(db);
+  // }, []);
+
+  useEffect(() => {
+    localStorage.setItem("cart", JSON.stringify(cart));
+  }, [cart]);
+
+  const addToCart = (item) => {
+    const itemExists = cart.findIndex((guitar) => guitar.id === item.id);
+    if (itemExists === -1) {
+      //no existe el producto en el carrito
+      item.quantity = 1;
+      return setCart((prevCart) => [...prevCart, item]);
+    } else {
+      //ya existe el producto en el carrito
+      const updateQuantity = [...cart];
+      updateQuantity[itemExists].quantity++;
+      setCart(updateQuantity);
+    }
+  };
+
+  const removeFromCart = (id) => {
+    setCart((prevCart) => prevCart.filter((item) => item.id !== id));
+  };
+
+  const incrementItem = (guitar) => {
+    const guitarToIncrement = cart.map((item) => {
+      if (item.id === guitar) {
+        return {
+          ...item,
+          quantity: item.quantity + 1,
+        };
+      }
+      return item;
+    });
+    setCart(guitarToIncrement);
+  };
+
+  const decrementItem = (id) => {
+    const guitarToDecrement = cart.map((item) => {
+      if (item.id === id && item.quantity > 1) {
+        return {
+          ...item,
+          quantity: item.quantity - 1,
+        };
+      }
+
+      return item;
+    });
+
+    setCart(guitarToDecrement);
+  };
+
+  const cleanCart = () => {
+    setCart([]);
+  };
+
+  return {
+    cart,
+    incrementItem,
+    decrementItem,
+    cleanCart,
+    addToCart,
+    removeFromCart,
+    guitar,
+  };
+};
+
+export default useCart;
