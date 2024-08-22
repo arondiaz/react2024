3bf6009a49d9befe1e6a13865a2ba70b29f2a9bd
Arón Díaz <arondiaz.cpn@gmail.com>
2024-08-22 14:22:58 -0300

    added initial view and form

--- calories-tracker/src/components/Form.jsx
+++ calories-tracker/src/components/Form.jsx
@@ -1,5 +1,52 @@
+import categories from "../data/categories";
+
 const Form = () => {
-  return <div>Form</div>;
+  console.log(categories);
+
+  return (
+    <form action="" className="space-y-5 bg-white shadow p-10 rounded-lg">
+      <div className="grid grid-cols-1 gap-3">
+        <label htmlFor="category">Categoría:</label>
+        <select
+          name=""
+          id="category"
+          className="border border-slate-300 p-2 rounded-sm bg-white w-full"
+        >
+          {categories.map((item) => (
+            <option key={item.id} value={item.id}>
+              {item.name}
+            </option>
+          ))}
+        </select>
+      </div>
+
+      <div className="grid grid-cols-1 gap-3">
+        <label htmlFor="activity">Actividad:</label>
+        <input
+          type="text"
+          id="activity"
+          className="border  border-slate-300 p-2 rounded-sm bg-white w-full"
+          placeholder="Ingrese la comida o el ejercicio"
+        />
+      </div>
+
+      <div className="grid grid-cols-1 gap-3">
+        <label htmlFor="calories">Calorías:</label>
+        <input
+          type="number"
+          id="calories"
+          className="border  border-slate-300 p-2 rounded-sm bg-white w-full"
+          placeholder="Ingrese el número de calorías"
+        />
+      </div>
+
+      <input
+        type="submit"
+        value="Agregar"
+        className="w-full bg-gray-600 cursor-pointer border rounded-lg text-white p-2 uppercase text-lg hover:bg-gray-500 ease-in-out"
+      />
+    </form>
+  );
 };
 
 export default Form;
---
+++ calories-tracker/src/data/categories.js
@@ -0,0 +1,6 @@
+const categories = [
+  { id: 1, name: "Comida" },
+  { id: 2, name: "Ejercicio" },
+];
+
+export default categories;
