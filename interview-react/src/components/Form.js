525d2f07a62d20f5f863aec86b946399c811df4f
Arón Díaz <arondiaz.cpn@gmail.com>
2024-08-23 11:14:02 -0300

    added validation and state on form

--- interview-react/src/components/Form.js
+++ interview-react/src/components/Form.js
@@ -1,20 +1,60 @@
 import React from "react";
+import { useState } from "react";
 
 const Form = () => {
+  const [loginData, setLoginData] = useState({
+    email: "",
+    password: "",
+  });
+
+  const handleChange = (e) => {
+    setLoginData({ ...loginData, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const { email, password } = loginData;
+
+    if (email.trim() === "" || password === "") {
+      return console.log("Campos vacíos");
+    }
+
+    const regex =
+      /^(([^<>()[\].,;:\s@"]+(\.[^<>()[\].,;:\s@"]+)*)|(".+"))@(([^<>()[\].,;:\s@"]+\.)+[^<>()[\].,;:\s@"]{2,})$/i;
+
+    if (!regex.test(email)) {
+      return console.log("Email incorrecto");
+    }
+
+    if (email !== "challenge@alkemy.org" || password !== "react") {
+      return console.log("Credenciales inválidas");
+    }
+  };
+
   return (
     <>
-      <form>
+      <form onSubmit={handleSubmit}>
         <label>
-          <span>  Ingrese su email  </span> <br />
-          <input type="text" name="email"/>
+          <span> Ingrese su email </span> <br />
+          <input
+            type="text"
+            name="email"
+            value={loginData.email}
+            onChange={handleChange}
+          />
         </label>
         <br />
-        <label> 
-          <span>  Ingrese su contraseña </span> <br/>
-          <input type="password" name="password"/>
+        <label>
+          <span> Ingrese su contraseña </span> <br />
+          <input
+            type="password"
+            name="password"
+            value={loginData.password}
+            onChange={handleChange}
+          />
         </label>
-        <br/>
-          <input type="submit" value={"Ingresar"}/>
+        <br />
+        <input type="submit" value={"Ingresar"} />
       </form>
     </>
   );
