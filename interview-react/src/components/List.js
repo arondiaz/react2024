d95ba1f678adca07adcf63a139a66c42f644df07
Arón Díaz <arondiaz.cpn@gmail.com>
2024-08-23 16:49:05 -0300

    added private route

--- interview-react/src/components/List.js
+++ interview-react/src/components/List.js
@@ -1,10 +1,20 @@
-
+import { useNavigate } from "react-router-dom";
+import { useEffect } from "react";
 
 const List = () => {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+
+    if (token === null) {
+      navigate("/");
+    }
+  }, [navigate]);
+
   return (
     <>
       <h1>Listadooo</h1>
-
     </>
   );
 };
