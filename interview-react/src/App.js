4a4da2adffd949ae6494b9e47417f77b9c3a7e91
Arón Díaz <arondiaz.cpn@gmail.com>
2024-08-24 19:10:31 -0300

    added boostrap

--- interview-react/src/App.js
+++ interview-react/src/App.js
@@ -3,17 +3,18 @@ import Form from "./components/Form";
 import List from "./components/List";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import "./css/bootstrap.min.css";
 
 function App() {
   return (
-    <>
+    <div className="container">
       <Header />
       <Routes>
         <Route path="/" element={<Form />} />
         <Route path="/list" element={<List />} />
       </Routes>
       <Footer />
-    </>
+    </div>
   );
 }
 
