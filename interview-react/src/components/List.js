33af303540ce577486572585605b22d816b1b178
Arón Díaz <arondiaz.cpn@gmail.com>
2024-08-25 00:15:35 -0300

    fetched data and implemented dotenv

--- interview-react/src/components/List.js
+++ interview-react/src/components/List.js
@@ -1,7 +1,23 @@
+import { useEffect, useState } from "react";
 import { Navigate } from "react-router-dom";
+import axios from "axios";
 
 const List = () => {
   let token = localStorage.getItem("token");
+
+  const [movieList, setMovieList] = useState([]);
+
+  useEffect(() => {
+    const apiKey = process.env.REACT_APP_API_KEY;
+    const url = `https://api.themoviedb.org/3/discover/movie?api_key=${apiKey}&language=en-US&page=1`;
+
+    axios.get(url).then((response) => {
+      const apiData = response.data;
+      setMovieList(apiData.results);
+    });
+  }, [setMovieList]);
+
+  console.log(movieList);
 
   if (!token) {
     return <Navigate to="/" />;
