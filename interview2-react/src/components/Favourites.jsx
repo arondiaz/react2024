54f0f749dde577f80e0003466db95f40f0f4c39d
Arón Díaz <arondiaz.cpn@gmail.com>
2024-09-03 22:02:20 -0300

    implemented view of favorites

--- interview2-react/src/components/Favourites.jsx
+++ interview2-react/src/components/Favourites.jsx
@@ -1,7 +1,51 @@
+import "../css/favourites.css";
+import { Navigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+
 const Favourites = () => {
+  let existFav = localStorage.getItem("favs");
+
+  let favsArray = existFav ? JSON.parse(existFav) : [];
+
+  const navigate = useNavigate();
+  // function goToDetail(id) {
+  //   const apiKey = import.meta.env.VITE_REACT_APP_API_KEY;
+  //   const url = `https://api.themoviedb.org/3/movie/${id}?api_key=${apiKey}&language=en-US`;
+
+  //   console.log(url);
+
+  //   async function callApi() {
+  //     const response = await fetch(url);
+  //     const request = await response.json();
+  //     console.log(request);
+  //   }
+
+  //   callApi();
+  // }
+
+  const redirectToDetails = (id) => {
+    navigate(`/detail?filmId=${id}`);
+  };
+
   return (
     <>
-      <h2>Favoritos</h2>
+      <h2 className="fav-title">Favoritos</h2>
+      <div className="container-fav">
+        {existFav.length > 0 ? (
+          favsArray.map((movFav) => (
+            <div className="card-fav" key={movFav.id}>
+              <img
+                className="fav-img"
+                src={movFav.img}
+                onClick={() => redirectToDetails(movFav.id)}
+              />
+              <h3 className="fav-title">{movFav.title}</h3>
+            </div>
+          ))
+        ) : (
+          <h3 className="fav-noresults">Sin resultados</h3>
+        )}
+      </div>
     </>
   );
 };
