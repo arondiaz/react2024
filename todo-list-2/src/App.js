6184ba05bea8d22bd7a1427f347a602a9a015e20
Arón Díaz <arondiaz.cpn@gmail.com>
2024-09-07 18:31:44 -0300

    added initial config for todo2 with tailwind

---
+++ todo-list-2/src/App.js
@@ -0,0 +1,13 @@
+import Title from "./components/Title";
+
+function App() {
+  return (
+    <div className="bg-gray-800 min-h-screen h-full text-gray-100 flex items-center justify-center py-20 px-5">
+      <div className="container flex flex-col max-w-xl">
+        <Title/>
+      </div>
+    </div>
+  );
+}
+
+export default App;
