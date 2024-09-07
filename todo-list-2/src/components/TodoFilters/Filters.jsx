cffdefdc9f6124372b1c15eb1dd7750a10199de4
Arón Díaz <arondiaz.cpn@gmail.com>
2024-09-07 19:54:54 -0300

    updated filters

--- todo-list-2/src/components/TodoFilters/Filters.jsx
+++ todo-list-2/src/components/TodoFilters/Filters.jsx
@@ -1,9 +1,23 @@
-import { FiltersContainer, ItemsLeft } from "./Filters.component";
+import {
+  FiltersContainer,
+  ItemsLeft,
+  FilterButtonContainer,
+  FilterButton,
+} from "./Filters.component";
 
 const Filters = () => {
   return (
     <FiltersContainer>
       <ItemsLeft />
+      <FilterButtonContainer>
+        <FilterButton action={() => {}} active="All" filter={"All"} />
+        <FilterButton action={() => {}} active="All" filter={"In progress"} />
+        <FilterButton action={() => {}} active="All" filter={"Finished"} />
+      </FilterButtonContainer>
+
+      <button className="text-gray-400 hover:text-white cursor-pointer transition-all duration-300 ease-in-out">
+        Clear All
+      </button>
     </FiltersContainer>
   );
 };
