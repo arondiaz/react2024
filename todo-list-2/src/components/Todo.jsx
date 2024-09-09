0baaffc1965cb64a653b55402445ba738850843d
Arón Díaz <arondiaz.cpn@gmail.com>
2024-09-09 15:10:54 -0300

    updated conditional on button to mark task as completed

--- todo-list-2/src/components/Todo.jsx
+++ todo-list-2/src/components/Todo.jsx
@@ -2,6 +2,7 @@ import React from "react";
 
 const Todo = ({ todo, handleSetComplete, handleSetDelete }) => {
   const { id, title, completed } = todo;
+  console.log(todo);
   return (
     <div className="flex items-center justify-between p-4 bg-gray-500 border-b border-solid border-gray-600">
       <div className="flex items-center">
@@ -10,11 +11,11 @@ const Todo = ({ todo, handleSetComplete, handleSetDelete }) => {
             onClick={() => handleSetComplete(id)}
             className="bg-green-700 border border-gray-700 border-solid p-3 mr-4 rounded-full cursor-pointer"
           >
-            <img
+            {/* <img
               src="/check-icon.svg"
               alt=""
               className="cursor-pointer h-3 w-3 transition-all duration-300 ease-in"
-            />
+            /> */}
           </div>
         ) : (
           <span
@@ -23,7 +24,7 @@ const Todo = ({ todo, handleSetComplete, handleSetDelete }) => {
           ></span>
         )}
 
-        <p className={"pl-3" + (completed && "line-through")}>{title}</p>
+        <p className={`pl-3" + ${completed && "line-through"}`}>{title}</p>
       </div>
       <img
         src="/close-icon.svg"
