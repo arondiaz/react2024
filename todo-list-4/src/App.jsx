198728155046f8ff5ebe6c4080a92cf064a004fa
Arón Díaz <arondiaz.cpn@gmail.com>
2024-09-13 16:43:56 -0300

    added new todo list project

---
+++ todo-list-4/src/App.jsx
@@ -0,0 +1,14 @@
+import Title from "./components/Title";
+import "./App.css";
+
+function App() {
+  return (
+    <div className=" bg-black flex justify-center items-center h-full min-h-screen ">
+      <div className="container flex flex-col justify-center items-center">
+        <Title />
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ todo-list-4/src/components/Title.jsx
@@ -0,0 +1,9 @@
+
+
+const Title = () => {
+  return (
+    <h1 className="text-white text-lg font-bold">Todo app</h1>
+  )
+}
+
+export default Title
